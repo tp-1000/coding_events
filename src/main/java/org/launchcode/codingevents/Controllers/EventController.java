f5a22399d493f0c237205f042029bf827a3f18e1
thomaspetty <thomaskpetty@gmail.com>
2020-05-25 12:17:41 -0500

    Simple form add, now able to dynamically add evetns fromt he page

--- src/main/java/org/launchcode/codingevents/Controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/Controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,20 +14,23 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    //lives at /events/create
+    //lives at /events/create   (for a get request deliver template events/create
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    @PostMapping("create")  // For a Post request to events/create (the initial GET returns the create.html) it will be redirected to the default events Get mapping.. (events/index) with the list
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:"; // redicrect to root
     }
 }
