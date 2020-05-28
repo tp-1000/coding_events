74380fd4b616741d3b379b447f94fa6ad26d3f3e
thomaspetty <thomaskpetty@gmail.com>
2020-05-28 14:31:30 -0500

    Added a unique Id for event object managment

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,12 +1,23 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
+    private int id;
+    private static int nextId = 1;
+
     private String name;
     private String description;
 
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId++;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 
     public String getDescription() {
@@ -23,5 +34,22 @@ public class Event {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
