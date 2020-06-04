5806a1c1b76ffe397149927f8ff100e71eb7ba34
thomaspetty <thomaskpetty@gmail.com>
2020-06-03 23:32:22 -0500

    Annotations added to the model fields, still needs controller setup to aplly rules defined in the annotations

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,23 +1,41 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
     private int id;
     private static int nextId = 1;
 
+    @Size(min = 3, max = 50, message = "Name must be between 3-50 characters")
+    @NotBlank
     private String name;
+    @Size(max = 500, message = "Description to long!")
     private String description;
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId++;
+
     }
 
     @Override
     public String toString() {
         return name;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public String getDescription() {
